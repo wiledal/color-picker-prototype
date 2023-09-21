66af39e9795dc456daa1e91c6caa8ef9b4ff848f
Hugo Wiledal <hugo.wiledal@gmail.com>
2023-09-21 09:39:57 +0200

    docs: more comments

--- src/lib/use-form-with-zod/use-form-with-zod.ts
+++ src/lib/use-form-with-zod/use-form-with-zod.ts
@@ -3,10 +3,17 @@ import { UseFormProps, useForm } from "react-hook-form";
 import { z } from "zod";
 
 interface UseFormWithZodOptions<TSchema extends z.AnyZodObject>
-  extends UseFormProps<z.infer<TSchema>> {
+  extends Omit<UseFormProps<z.infer<TSchema>>, "resolver"> {
+  /**
+   * A z.object used for validation of the form.
+   */
   schema: TSchema;
 }
 
+/**
+ * An extended `useForm` that incorporates Zod validation in the `schema`, inferring types
+ * for type safe values and errors.
+ */
 export const useFormWithZod = <TSchema extends z.AnyZodObject>({
   schema,
   ...options
